6c0a77638a3b17e9bc04498b312f15b15eca70a2
Elia Etzanidi <eliaetz@gmail.com>
2025-10-01 16:46:41 +0300

    created home page components files

---
+++ client/src/components/gallery/gallery.jsx
@@ -0,0 +1,9 @@
+import './gallery.css'
+
+const gallery = () => {
+  return (
+    <div className=''>gallery</div>
+  )
+}
+
+export default gallery
---
+++ client/src/components/topBar/topBar.jsx
@@ -0,0 +1,9 @@
+import './topBar.css'
+
+const topBar = () => {
+  return (
+    <div className='topBar'>topBar</div>
+  )
+}
+
+export default topBar
