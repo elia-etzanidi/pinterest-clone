283d1ef5dd6376e53326dfb874322277e1b24e4c
Elia Etzanidi <eliaetz@gmail.com>
2025-10-02 23:06:12 +0300

    image component

--- client/src/components/galleryItem/galleryItem.jsx
+++ client/src/components/galleryItem/galleryItem.jsx
@@ -1,10 +1,14 @@
 import './galleryItem.css'
 import { Link } from 'react-router'
+import Image from '../image/image'
 
-const galleryItem = ({item}) => {
+const galleryItem = ({ item }) => {
+
+  const optimizedHeight = (372 * item.height) / item.width
+
   return (
     <div className='galleryItem' style={{gridRowEnd: `span ${Math.ceil(item.height / 100)}`}}>
-        <img src={item.media} alt="" />
+        <Image src={item.media} alt="" w={372} h={optimizedHeight} />
         <Link to={`/pin/${item.id}`} className='overlay' />
         <button className='saveButton'>Save</button>
         <div className='overlayIcons'>
