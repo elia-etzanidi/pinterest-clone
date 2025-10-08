374b7d14c37f0ed99b2dbdf0dcd737fe21df1de7
Elia Etzanidi <eliaetz@gmail.com>
2025-10-08 16:57:24 +0300

    post interactions

--- client/src/components/postInteractions/postInteractions.jsx
+++ client/src/components/postInteractions/postInteractions.jsx
@@ -1,8 +1,17 @@
 import './postInteractions.css'
+import Image from '../image/Image'
 
 const postInteractions = () => {
   return (
-    <div className='postInteractions'>postInteractions</div>
+    <div className='postInteractions'>
+      <div className="interactionIcons">
+        <Image src="/general/react.svg" />
+        273
+        <Image src="/general/share.svg" />
+        <Image src="/general/more.svg" />
+      </div>
+      <button>Save</button>
+    </div>
   )
 }
 
