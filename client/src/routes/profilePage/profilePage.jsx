aec7e8d0b19216ce6a56cc22d1311220e5d62359
Elia Etzanidi <eliaetz@gmail.com>
2025-10-09 14:26:17 +0300

    profile page information

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,8 +1,37 @@
 import './profilePage.css'
+import Image from '../../components/image/Image'
+import { useState } from 'react'
 
 const profilePage = () => {
+
+const [type, setType] = useState('saved')
+
   return (
-    <div className='profilePage'>profilePage</div>
+    <div className='profilePage'>
+      <Image 
+        className="profileImg" 
+        w={100}
+        h={100}
+        src="/general/noAvatar.png" 
+        alt="" 
+      />
+      <h1 className='profileName'>John Doe</h1>
+      <span className='profileUsername'>@jonhdoe</span>
+      <div className="followCount">10 followers • 20 following</div>
+      <div className="profileInteractions">
+        <Image src="/general/share.svg" alt="" />
+        <div className="profileButtons">
+          <button>Message</button>
+        <button>Follow</button>
+        </div>
+        <Image src="/general/more.svg" alt="" />
+      </div>
+      <div className="profileOptions">
+        <span onClick={() => setType("created")} className={type==="created" ? "active" : ""}>Created</span>
+        <span onClick={() => setType("saved")} className={type==="saved" ? "active" : ""}>Saved</span>
+      </div>
+    </div>
+
   )
 }
 
