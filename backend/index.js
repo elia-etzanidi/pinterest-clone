f69fef4fe160d2335ba42a71cb70990eebee4a0c
Elia Etzanidi <eliaetz@gmail.com>
2025-10-14 17:52:56 +0300

    added test routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 //running our express api using this file
 
 import express from 'express'
+import userRouter from './routes/user.route.js'
+import pinRouter from './routes/pin.route.js'
+import commentRouter from './routes/comment.route.js'
+import boardRouter from './routes/board.route.js'
 
 const app = express();
 
-app.use("/test", (req,res)=> {
-    return res.json("Hello from backend API")
-})
+app.use("/users", userRouter)
+app.use("/pins", pinRouter)
+app.use("/comments", commentRouter)
+app.use("/boards", boardRouter)
 
 app.listen(3000, () => {
     console.log("Server is running!")
