f8ce9b0703712f53cc6b21a570fe0e66655cb9d3
Elia Etzanidi <eliaetz@gmail.com>
2025-11-25 14:15:43 +0200

    options with colorpicker

--- client/src/components/editor/Options.jsx
+++ client/src/components/editor/Options.jsx
@@ -1,8 +1,53 @@
+import useEditorStore from "../../utils/editorStore"
+import { HexColorPicker } from "react-colorful";
+import { useState } from "react";
 
 const Options = () => {
-  return (
-    <div className="options">Options</div>
-  )
-}
 
-export default Options
+  const { selectedLayer, textOptions, setTextOptions } = useEditorStore();
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+
+  return (
+    <div className="options">
+      {selectedLayer === "text" ? (
+        <div className="">
+          <div className="editingOption">
+            <span>Font Size</span>
+            <input 
+              type="number" 
+              value={textOptions.fontSize} 
+              onChange={(e) => 
+                setTextOptions({...textOptions, fontSize: e.target.value})
+              }
+            />
+          </div>
+          <div className="editingOption">
+            <span>Color</span>
+            <div className="textColor">
+              <div 
+                className="colorPreview" 
+                style={{ backgroundColor: textOptions.color }}
+                onClick={() => setIsColorPickerOpen((prev) => !prev)}
+              /> 
+            </div>
+            {isColorPickerOpen && (
+              <div className="colorPicker">
+                <HexColorPicker 
+                  color={textOptions.color} 
+                  onChange={(color) => setTextOptions({...textOptions, color})
+                  }
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="">
+
+        </div>
+      )} 
+    </div>
+  );
+};
+
+export default Options;
