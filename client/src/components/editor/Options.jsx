deb848c5a8b265139b780d006ee615f9a485d098
Elia Etzanidi <eliaetz@gmail.com>
2025-12-01 17:20:37 +0200

    changes to photo editing

--- client/src/components/editor/Options.jsx
+++ client/src/components/editor/Options.jsx
@@ -72,12 +72,19 @@ const Options = ({ previewImg}) => {
 
   const { selectedLayer, textOptions, setTextOptions, canvasOptions, setCanvasOptions } = useEditorStore();
   const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+  const originalOrientation = previewImg.width < previewImg.height ? "portrait" : "landscape";
 
   const handleOrientationClick = (orientation) => {
-    const newHeight = 
-      orientation === "portrait" 
-      ? (375 * previewImg.width) / previewImg.height 
-      : (375 * previewImg.height) / previewImg.width;
+    let newHeight;
+
+    if(
+      (originalOrientation === "portrait" && orientation==="portrait") || 
+      (originalOrientation === "landscape" && orientation==="landscape")
+    ){
+      newHeight = (375 * previewImg.height) / previewImg.width;
+    } else {
+      newHeight = (375 * previewImg.width) / previewImg.height;
+    }
 
       setCanvasOptions({
         ...canvasOptions,
@@ -93,10 +100,15 @@ const Options = ({ previewImg}) => {
     
     if(size==="original"){
 
-      if(canvasOptions.orientation==="portrait"){
-        newHeight = (375 * previewImg.width) / previewImg.height;
-      } else {
+      if(
+        (originalOrientation === "portrait" && 
+          canvasOptions.orientation==="portrait") || 
+        (originalOrientation === "landscape" && 
+          canvasOptions.orientation==="landscape")
+      ){
         newHeight = (375 * previewImg.height) / previewImg.width;
+      } else {
+        newHeight = (375 * previewImg.width) / previewImg.height;
       }
 
     }else {
